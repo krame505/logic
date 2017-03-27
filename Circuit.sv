45146a0fda4903e3b49dbb5c58045c768bc541f8
Lucas Kramer <krame505@umn.edu>
2017-03-26 22:24:06 -0500

    Improved dot rendering

--- Circuit.sv
+++ Circuit.sv
@@ -74,8 +74,8 @@ top::Circuit ::= cs::Circuits
   top.dotLName = "parallel_" ++ toString(genInt());
   top.dotRName = "parallel_" ++ toString(genInt());
   top.dotPP =
-    s"  ${top.dotLName} [shape=point];\n  ${top.dotRName} [shape=point];" ++
-    cs.parallelDotPP;
+    cs.parallelDotPP ++
+    s"  ${top.dotLName} [shape=point];\n  ${top.dotRName} [shape=point];\n";
   cs.parallelDotLNameIn = top.dotLName;
   cs.parallelDotRNameIn = top.dotRName;
 
@@ -98,10 +98,11 @@ top::Circuits ::= h::Circuit t::Circuits
 {
   top.seriesDotPP = h.dotPP ++ t.seriesDotPP ++ s"  ${h.dotRName} -- ${t.dotLName};\n";
   top.parallelDotPP =
-    s"  ${top.parallelDotLNameIn} -- ${h.dotLName};\n" ++
-    h.dotPP ++
-    s"  ${h.dotRName} -- ${top.parallelDotRNameIn};\n" ++
-    t.parallelDotPP;
+    h.dotPP ++ t.parallelDotPP ++
+s"""  { rank=same; ${h.dotLName} ${t.dotLName} }
+  { rank=same; ${h.dotRName} ${t.dotRName} }
+  ${top.parallelDotLNameIn} -- ${h.dotLName};
+  ${h.dotRName} -- ${top.parallelDotRNameIn};""";
   top.dotLName = h.dotLName;
   top.dotRName = t.dotRName;
   top.toAndExpr = andExpr(h.toExpr, t.toAndExpr);
@@ -113,9 +114,8 @@ top::Circuits ::= h::Circuit
 {
   top.seriesDotPP = h.dotPP;
   top.parallelDotPP =
-    s"  ${top.parallelDotLNameIn} -- ${h.dotLName};\n" ++
     h.dotPP ++
-    s"  ${h.dotRName} -- ${top.parallelDotRNameIn};\n";
+    s"  ${top.parallelDotLNameIn} -- ${h.dotLName};\n  ${h.dotRName} -- ${top.parallelDotRNameIn};";
   top.dotLName = h.dotLName;
   top.dotRName = h.dotRName;
   top.toAndExpr = h.toExpr;
@@ -127,7 +127,7 @@ String ::= c::MultiCircuit
 {
   return s"""
 graph {
-graph [splines=ortho];
+  graph [rankdir=LR];
 ${c.dotPP}
 }""";
 }
